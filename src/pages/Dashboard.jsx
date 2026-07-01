70e2b27f4cd2c8956789034aebd269aa6b844263
daacad1234 <haruundaacad91@gmail.com>
2026-07-01 13:58:02 +0300

    Update latest changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {FaHouse,FaBook,FaArrowRight,FaUsers,FaGear,FaChartColumn,} from "react-icons/fa6";
+import {FaBook,FaArrowRight,} from "react-icons/fa6";
 import api from '../api';
 
 function Dashboard() {
